d73c7b1cfd264916217007e0e77b5ee154d6b4f7
Igor <igrfclima@gmail.com>
2025-09-30 18:20:40 -0300

    feat: add array 1d and 2d

---
+++ Month 1/Week 1/day_one.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def array_operations():
+    # 1D array
+    arr_1d = np.array([1,2,3,4,5])
+    print("1D Array:")
+    print(arr_1d)
+
+    # 2D array
+    arr_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+    print("2D Array:")
+    print(arr_2d)
+
+def main():
+    array_operations()
+
+if __name__ == "__main__":
+    main()
