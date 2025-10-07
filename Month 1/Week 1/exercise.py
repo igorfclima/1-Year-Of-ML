5b2d24574bbf77cc1269eb7f0386218fdbd234ae
Igor <igrfclima@gmail.com>
2025-10-07 18:32:24 -0300

    feat: add random_notes function

--- Month 1/Week 1/exercise.py
+++ Month 1/Week 1/exercise.py
@@ -1,2 +1,12 @@
 import numpy as np
 import random as rd
+
+def random_notes():
+    student_one = [rd.randint(0, 10) for _ in range(4)]
+    student_two = [rd.randint(0, 10) for _ in range(4)]
+    student_three = [rd.randint(0, 10) for _ in range(4)]
+    student_four = [rd.randint(0, 10) for _ in range(4)]
+    return np.array([student_one,
+                     student_two,
+                     student_three,
+                     student_four])
